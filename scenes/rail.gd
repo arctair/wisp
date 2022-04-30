05c40b07b0c6011e23d9d941ce53dfcbaee2cda5
tyler <tyler@cruftbusters.com>
2022-04-30 11:22:48 -0700

    duplicate rails, simplomatic can snap to different rails, simplomatics move with rails

---
+++ scenes/rail.gd
@@ -0,0 +1,3 @@
+extends Area
+
+export var unitCount : int
--- scenes/scene.gd
+++ scenes/scene.gd
@@ -12,19 +12,61 @@ func _ready():
 
 
 func _physics_process(delta):
-	if not Input.is_action_pressed("left_mouse_button"):
+	process_click_and_drag(delta)
+	process_duplicate(delta)
+
+var selected : Spatial
+func process_click_and_drag(delta):
+	if Input.is_action_just_pressed("click_and_drag"):
+		var container = intersect_mouse_ray()[0]
+		if not container: return
+		selected = container.collider
+		
+	if Input.is_action_just_released("click_and_drag") or Input.is_action_just_released("duplicate"):
+		selected = null
+	
+	if not selected: return
+	
+	var mouse_ray = intersect_mouse_ray([selected])
+	var container = mouse_ray[0]
+	var mouse_position_3d = mouse_ray[1]
+	var mouse_normal = mouse_ray[2]
+	
+	if not container or container.collider.name != "rail":
+		set_parent(selected, self)
+		selected.transform.origin = mouse_position_3d + mouse_normal * 1
+		return
+		
+	var rail = container.collider as Spatial
+	var metersPerUnit = 0.04445
+	var unitCount = rail.get("unitCount")
+	var u = round((container.position.y - rail.transform.origin.y) / metersPerUnit - 0.5)
+	u = clamp(u, -unitCount / 2, unitCount / 2 - 1)
+	set_parent(selected, rail)
+	selected.transform.origin = Vector3.UP * (u + 0.5) * metersPerUnit
+
+func set_parent(target, parent):
+	if target.get_parent() == parent: return
+	target.get_parent().remove_child(target)
+	parent.add_child(target)
+
+
+func process_duplicate(delta): 
+	if not Input.is_action_just_pressed("duplicate"):
 		return
 	
-	var camera = $Camera
+	var container = intersect_mouse_ray()[0]
+	
+	if not container: return
+		
+	var spatial = container.collider as Spatial
+	selected = spatial
+	spatial.get_parent().add_child(spatial.duplicate())
+
+func intersect_mouse_ray(exclude = [], camera = $Camera, distance = 1000):
 	var mouse_position = get_viewport().get_mouse_position()
 	var from = camera.project_ray_origin(mouse_position)
-	var to = from + camera.project_ray_normal(mouse_position) * 1000
+	var normal = camera.project_ray_normal(mouse_position)
+	var to = from + normal * distance
 	var space_state = get_world().direct_space_state
-	var container = space_state.intersect_ray(from, to, [], 0x7FFFFFFF, true, true)
-	
-	if container and container.collider == $rail_12u/Area:
-		var u = round((container.position.y - $rail_12u.transform.origin.y) / 0.04445 - 0.5) + 7
-		u = clamp(u, 1, 12)
-		$rail_12u/simplomatic.transform.origin = Vector3(0, (u - 7) * 0.04445 + 0.04445 / 2, 0)
-	else:
-		$rail_12u/simplomatic.transform.origin = from + camera.project_ray_normal(mouse_position) * 1
+	return [space_state.intersect_ray(from, to, exclude, 0x7FFFFFFF, true, true), from, normal]
